2b7d24e08b9db6df6d02c8bf80abfadc433f0e88
RecklessCode-Y <nicoyunghan@gmail.com>
2026-06-07 17:01:30 +0700

    Enhance run.bat to verify virtual environment
    Add checks for virtual environment existence before running the script.

--- run.bat
+++ run.bat
@@ -1,5 +1,14 @@
 @echo off
 
+cd /d "%~dp0"
+
+if not exist handmotion_mouse (
+echo Virtual environment not found.
+echo Please run setup.bat first.
+pause
+exit /b
+)
+
 call handmotion_mouse\Scripts\activate
 
 python hand_trackv6.py
